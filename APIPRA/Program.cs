ec05112753bc5b7a5379111dd2725598d2cd1671
Cockepinle <144021431+Cockepinle@users.noreply.github.com>
2025-05-21 00:51:06 +0300

    Update Program.cs

--- APIPRA/Program.cs
+++ APIPRA/Program.cs
@@ -3,6 +3,15 @@ using APIPRA.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Получаем порт из переменной окружения (если нет — 8080 по умолчанию)
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+// Настраиваем Kestrel, чтобы слушать этот порт
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(port));
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
