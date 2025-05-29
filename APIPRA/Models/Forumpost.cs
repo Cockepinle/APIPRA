e3e52c92b40a55771deaa1bb9932e0d879a30b5b
Cockepinle <144021431+Cockepinle@users.noreply.github.com>
2025-05-29 18:48:50 +0300

    Update Forumpost.cs

--- APIPRA/Models/Forumpost.cs
+++ APIPRA/Models/Forumpost.cs
@@ -7,14 +7,12 @@ public partial class Forumpost
 {
     public int Id { get; set; }
 
-    public int? UserId { get; set; }
+    public int UserId { get; set; }
 
     public string Title { get; set; } = null!;
 
     public string Content { get; set; } = null!;
 
     public DateTime? CreatedAt { get; set; }
-
-    public virtual User? User { get; set; }
 }
 
